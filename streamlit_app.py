dc9081d0806c6ae7d7a742e8604f730887672c3a
Sima Dutta <sdutta7@charlotte.edu>
2024-10-01 23:13:09 -0400

    file path updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import math
 st.title("Data App Assignment, on Oct 7th")
 
 st.write("### Input Data and Examples")
-df = pd.read_csv("C:/Users/simmi/OneDrive/Desktop/5122-Streamlit/Superstore_Sales_utf8.csv", parse_dates=True)     
+#df = pd.read_csv("C:/Users/simmi/OneDrive/Desktop/5122-Streamlit/Superstore_Sales_utf8.csv", parse_dates=True)
+df = pd.read_csv("Superstore_Sales_utf8.csv", parse_dates=True)     
 st.dataframe(df)
 
 # This bar chart will not have solid bars--but lines--because the detail data is being graphed independently
